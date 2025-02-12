b3dabab1595b517378e09416a46faed5b5cb3d6a
Yerin Park <95178362+piaoruilin@users.noreply.github.com>
2025-02-12 17:31:57 +0800

    hello world

--- projects/helloworld/helloworld.cpp
+++ projects/helloworld/helloworld.cpp
@@ -1,6 +1,33 @@
-#include <iostream>
+#include <iostream> //this is needed for std::cout and stt:endl//
 
+/*
+//helloworld
 int main()
 {
-    std::cout << "Hello World" << std::endl;
+    std::cout << "Hello World" << std::endl; //still dk what's std::endl
+}
+
+//define integers
+int main()
+{
+    int x; //define 1 integer
+    int y, z; //define 2 integers
+
+    return 0;
+}
+*/
+
+//assignment of integers
+int main()
+{
+    int width;
+    width = 5;
+
+    std::cout << width; //prints 5
+
+    width = 7;
+
+    std::cout << width; //prints 57
+
+    return 0;
 }
